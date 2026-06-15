2415607e38c13eabd284a6beff2e8fb6b646a615
SteveJnr05 <swantastephen@gmail.com>
2026-06-15 11:36:48 +0100

    Update

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
 
 
 class Question(models.Model):
@@ -9,6 +10,10 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now- datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('choice/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='result'),
-    path('<int:question_id>/vote/', views.vote, name='vote' )
+    path('', views.IndexView.as_view(), name="index"),
+    path('choice/', views.ChoiceView.as_view(), name='choice'),
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.Vote.as_view(), name='vote' )
 ]
